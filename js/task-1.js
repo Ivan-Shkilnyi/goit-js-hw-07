547d33b8c56340d91e3364a61770c033fed9cff0
John Shkilnyi <ivan.shkilnyi@gmail.com>
2026-02-17 23:29:01 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,9 @@
 const categoriesList = document.querySelector('#categories');
 const categories = categoriesList.querySelectorAll('.item');
 console.log(`Numbers of ategories : ${categories.length}`);
+categories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+    const elements = category.querySelectorAll('ul li').length;
+    console.log(`Category : ${title}`);
+    console.log(`Elemebts : ${elements}`)
+});
